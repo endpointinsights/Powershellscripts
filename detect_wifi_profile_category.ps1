cae9f7312b1615294b2614b53058c71f11f0d3c4
Mubashir Javaid <mubashirjavaid@gmail.com>
2024-04-01 01:57:18 -0700

    Add files via upload

---
+++ detect_wifi_profile_category.ps1
@@ -0,0 +1,29 @@
+﻿# Detect if Vejessiens Wifi is public or private and highlight
+# MUbashir Javaid
+# 29 August, 2023
+# Exit code 0 = configuration is correct.
+# Exit code 1 = configuration is incorrect, trigger remediation script.
+
+$ErrorActionPreference = "SilentlyContinue"
+try
+{
+    $NetworkCategory = Get-NetConnectionProfile -Name "Vejessiens" | select NetworkCategory 
+    Write-Output $NetworkCategory.NetworkCategory
+
+    if($NetworkCategory.NetworkCategory -eq "Public") 
+    {
+        Write-Output "Vejession WiFi profile is public"
+        Exit 1
+    }
+    else 
+    {
+        Write-Output "Vejession Wifi profile is private"
+        Exit 0
+    }
+
+} 
+catch
+{
+    Write-Output "Vejession Wifi profile Not found"
+    Exit 1
+}
---
+++ remediate_wifi_profile_category.ps1
@@ -0,0 +1,30 @@
+﻿# Remediate if Vejessiens Wifi is public or private
+# MUbashir Javaid
+# 29 August, 2023
+# Exit code 0 = configuration is correct.
+# Exit code 1 = configuration is incorrect, trigger remediation script.
+
+$ErrorActionPreference = "SilentlyContinue"
+try
+{
+    $NetworkCategory = Get-NetConnectionProfile -Name "Vejessiens" | select NetworkCategory 
+    Write-Output $NetworkCategory.NetworkCategory
+
+    if($NetworkCategory.NetworkCategory -eq "Public") 
+    {
+        Set-NetConnectionProfile -Name "Vejessiens" -NetworkCategory Private
+        Write-Output "Vejession Wifi profile is change from public to private"
+        Exit 0
+    }
+    else 
+    {
+         Write-Output "Vejession Wifi profile is not changed, It's already public profile."
+         Exit 1
+    }
+
+} 
+catch
+{
+    Write-Output "Vejession Wifi profile Not found"
+    Exit 1
+}
